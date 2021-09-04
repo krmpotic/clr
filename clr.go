943571678b427a14d40480bc67316fe7ee72b566
Luka Krmpotic <luka.krmpotic@cosylab.com>
2021-09-04 23:08:59 +0200

    color first argument red

---
+++ clr.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"regexp"
+)
+
+const (
+	esc   = "\x1b"
+	reset = esc + "[0m" // reset; clears all colors and styles (to white on black)
+
+	bold_on      = esc + "[1m" // bold on
+	italics_on   = esc + "[3m" // italics on
+	underline_on = esc + "[4m" // underline on
+	inverse_on   = esc + "[7m" // inverse on; reverses foreground & background colors
+	strike_on    = esc + "[9m" // strikethrough on
+
+	bold_off      = esc + "[22m" // bold off
+	italics_off   = esc + "[23m" // italics off
+	underline_off = esc + "[24m" // underline off
+	inverse_off   = esc + "[27m" // inverse off
+	strike_off    = esc + "[29m" // strikethrough off
+
+	fg_black   = esc + "[30m" // set foreground color to black
+	fg_red     = esc + "[31m" // set foreground color to red
+	fg_green   = esc + "[32m" // set foreground color to green
+	fg_yellow  = esc + "[33m" // set foreground color to yellow
+	fg_blue    = esc + "[34m" // set foreground color to blue
+	fg_magenta = esc + "[35m" // set foreground color to magenta (purple)
+	fg_cyan    = esc + "[36m" // set foreground color to cyan
+	fg_white   = esc + "[37m" // set foreground color to white
+	fg_default = esc + "[39m" // set foreground color to default (white)
+
+	bg_black   = esc + "[40m" // set background color to black
+	bg_red     = esc + "[41m" // set background color to red
+	bg_green   = esc + "[42m" // set background color to green
+	bg_yellow  = esc + "[43m" // set background color to yellow
+	bg_blue    = esc + "[44m" // set background color to blue
+	bg_magenta = esc + "[45m" // set background color to magenta (purple)
+	bg_cyan    = esc + "[46m" // set background color to cyan
+	bg_white   = esc + "[47m" // set background color to white
+	bg_default = esc + "[49m" // set background color to default (black)
+)
+
+func main() {
+	var s string
+	if len(os.Args) < 2 {
+		os.Exit(1)
+	} else {
+		s = os.Args[1]
+	}
+	re := regexp.MustCompile(s)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		i := 0
+		t := scanner.Text()
+		for _, m := range re.FindAllStringIndex(t, -1) {
+			fmt.Printf("%s", t[i:m[0]])
+			fmt.Printf("%s", fg_red)
+			fmt.Printf("%s", t[m[0]:m[1]])
+			fmt.Printf("%s", reset)
+			i = m[1]
+		}
+		fmt.Println(t[i:])
+	}
+}
